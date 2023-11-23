b7fddd045b6519e863184cc34bcc99e9fd6e527f
Clazariuk1 <Clazariuk@gmail.com>
2023-11-23 13:16:54 -0800

    Game debugged and working, awaiting win-tally frequency counter.

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
+// (function() {
 
-
-// 1) Define required constants:
-//   1.1) Define a colors object with keys of 'null' (when the square is empty), and players 1 & -1. The value assigned to each key represents the color to display for an empty square (null), player 1 and player -1.
-//   1.2) Define the 8 possible winning combinations, each containing three indexes of the board that make a winner if they hold the same player value.
-
-const colors = {
-  '1': 'blue',
-  '-1': 'red',
-'null': 'gray'
+const LOOKUP = {
+  '1': 'X',
+  '-1': 'O',
+  'null': ''
 }
 
 const winningCombos = [
@@ -21,33 +17,38 @@ const winningCombos = [
   [2, 4, 6]
 ];
 
-let board, turn, winner;
+let gameBoard, turn, winner;
 
 const display = document.getElementById("display");
-// const gameBoard = document.getElementById('game-board')
-// const gameButtons = document.querySelectorAll('#game-btn');
+const gameBoardEl = document.getElementById('game-board')
+const squareEls = document.querySelectorAll('.square');
 const replayBtn = document.getElementById('replay');
+const Xwins = document.getElementById('X-wins');
+const Owins = document.getElementById('O-wins');
+const gameTally = document.getElementById('game-tally');
+let gamesTally = 0;
+let XWINS = 0;
+let OWINS = 0;
 
-document.getElementById('game-board').addEventListener("click", playerMove);
-replayBtn.addEventListner("click", startGame());
+gameBoardEl.addEventListener("click", playerMove);
+replayBtn.addEventListener("click", startGame, renderTally());
 
 startGame();
 
 function startGame() {
-  board = [null, null, null, null, null, null, null, null, null];
+  gamesTally++;
+  gameBoard = [null, null, null,
+               null, null, null,
+               null, null, null];
   turn = 1;
-  winner = null;
+  winner = false;
   render();
 }
 
 function playerMove(event) {
-  const idx = parseInt(event.target.id.replace('square-', ''));
-  if (
-    isNaN(idx) ||
-
-    winner
-  ) return;
-  board[idx] = turn;
+  const position = event.target.dataset.index;
+  if (winner || gameBoard[position] !== null) return;
+  gameBoard[position] = turn;
   turn *= -1;
   winner = renderWinner();
   render();
@@ -55,9 +56,12 @@ function playerMove(event) {
 
 function renderWinner() {
   for (let winningArray of winningCombos) {
-    if (Math.abs(board[winningArray[0]] + board[winningArray[1]] + board[winningArray[2]]) === 3) return board[winningArray[0]];
+    if (Math.abs(gameBoard[winningArray[0]] + gameBoard[winningArray[1]] + gameBoard[winningArray[2]]) === 3) {
+      gamesTally++;
+      return gameBoard[winningArray[0]];
+    }
   }
-  if (board.includes(null)) return null;
+  if (gameBoard.includes(null)) return false;
   return 'T';
 }
 
@@ -68,10 +72,9 @@ function render() {
 }
 
 function renderBoard() {
-  board.forEach(function(squareVal, index) {
-    const square = document.querySelector(`square__${index}`);
-    square.style.backgroundColor = colors[squareVal];
-    square.className = !squareVal ? 'avail' : '';
+  squareEls.forEach(function(square, position) {
+    square.textContent = LOOKUP[gameBoard[position]];
+
   });
 }
 
@@ -79,67 +82,49 @@ function renderDisplay() {
   if (winner === 'T') {
     display.innerHTML = `Game Over; players are tied!`;
   } else if (winner) {
-    display.innerHTML = `Congratulations <span style="color: ${colors[winner]}">${colors[winner].toUpperCase()}</span>!`;
+    display.innerHTML = `Congratulations <span style="Current Player: ${LOOKUP[winner]}">${LOOKUP[winner].toUpperCase()}</span>!`;
   } else {
-    display.innerHTML = `<span style="color: ${colors[turn]}">${colors[turn].toUpperCase()}</span>'s turn.`
+    display.innerHTML = `<span style="Current Player: ${LOOKUP[turn]}">${LOOKUP[turn].toUpperCase()}</span>'s turn.`
   }
 }
 
-// engineer playerTurn = X on reset button click.
-// const counter = turnCounter();
-// function turnCounter() {
-//   let turnCount = 0;
-//   gameButtons.addEventListener("click", () => {
-//     turnCount++;
-//   })
-//   return turnCount;
-// }
-// return function(action) {
-//   if (action === "gameButtons") {
-//     turnCount++;
-//   } else if (action === "replayButton") {
-//     turnCount === 0;
+ function renderTally() {
+//   if (winner) {
+//     if (${LOOKUP[winner]} === LOOKUP[0]) {
+//       XWINS++;
+//     } else if (${LOOKUP[winner]} === LOOKUP[1]) {
+//       OWINS++;
+//     }
+//     if (XWINS === 3) {
+//       display.innerHTML = `Player X has won the best-of-three Championship!!`
+//     } else if (OWINS === 3) {
+//       display.innerHTML = `Player O has won the best-of-three Championship!!`
+//     } else if (gamesTally === 5) {
+//       display.innerHTML = `This tournament is a deadlocked tie! Refresh the page to start over.`
+//     }
+    gameTally.innerHTML = `${gamesTally}`;
+//     XWins.innerHTML = `${XWINS}`;
+//     OWins.innerHTML = `${OWINS}`;
+//     return;
+//     }
+ }
+
+// renderTally() {
+//   let grandChampion = 3;
+//   tallyCounter();
+//   Xwins.innerHTML = `${XWINS}`;
+//   Owins.innerHTML = `${OWINS}`;
+
+//   if (XWINS === grandChampion) {
+//     display.innerHTML = `Player X has won the best-of-three Championship!!`
+//   } else if (OWINS === grandChampion) {
+//     display.innerHTML = `Player O has won the best-of-three Championship!!`
 //   }
-//   return turnCount;
-// };
+
+//   function tallyCounter(winner) {
+//   if (winner === '1') {
+//     XWINS += 1;
+//   } else if (winner === '-1') {
+//     OWINS += 1;
+//   }
 // }
-
-
-// 4) Upon loading the app should:
-//   4.1) Initialize the state variables:
-//     4.1.1) Initialize the board array to 9 nulls to represent empty squares. The 9 elements will "map" to each square, where index 0 maps to the top-left square and index 8 maps to the bottom-right square.
-//     4.1.2) Initialize whose turn it is to 1 (player 'X'). Player 'O' will be represented by -1.
-//     4.1.3) Initialize winner to null to represent that there is no winner or tie yet. Winner will hold the player value (1 or -1) if there's a winner. Winner will hold a 'T' if there's a tie.
-//   4.2) Render those state variables to the page:
-
-
-//     4.2.1) Render the board:
-//       4.2.1.1) Loop over each of the 9 elements that represent the squares on the page, and for each iteration:
-//         4.2.1.1.2) Use the index of the iteration to access the mapped value from the board array.
-//         4.3.1.1.3) Set the background color of the current element by using the value as a key on the colors lookup object (constant).
-//     4.2.2) Render a message:
-//       4.2.2.1) If winner has a value other than null (game still in progress), render whose turn it is - use the color name for the player, converting it to upper case.
-//       4.2.2.2) If winner is equal to 'T' (tie), render a tie message.
-//       4.2.2.3) Otherwise, render a congratulatory message to which player has won - use the color name for the player, converting it to uppercase.
-//   4.3) Wait for the user to click a square
-
-// 5) Handle a player clicking a square:
-//   5.1) Obtain the index of the square that was clicked by either:
-//     5.1.1) "Extracting" the index from an id assigned to the element in the HTML, or
-//     5.1.2) Looping through the cached square elements using a for loop and breaking out when the current square element equals the event object's target.
-//   5.2) If the board has a value at the index, immediately return because that square is already taken.
-//   5.3) If winner is not null, immediately return because the game is over.
-//   5.4) Update the board array at the index with the value of turn.
-//   5.5) Flip turns by multiplying turn by -1 (flips a 1 to -1, and vice-versa).
-//   5.6) Set the winner variable if there's a winner:
-//     5.6.1) Loop through the each of the winning combination arrays defined.
-//     5.6.2) Total up the three board positions using the three indexes in the current combo.
-//     5.6.3) Convert the total to an absolute value (convert any negative total to positive).
-//     5.6.4) If the total equals 3, we have a winner! Set winner to the board's value at the index specified by the first index in the combo array. Exit the loop.
-//   5.7) If there's no winner, check if there's a tie:
-//     5.7.1) Set winner to 'T' if there are no more nulls in the board array.
-//   5.8) All state has been updated, so render the state to the page (step 4.2).
-
-
-// 6) Handle a player clicking the replay button:
-//   6.1) Do steps 4.1 (initialize the state variables) and 4.2 (render).
